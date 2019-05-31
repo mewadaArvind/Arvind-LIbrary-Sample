cb32b2354ae8b6410bc56e40579a831a89ca4749
Arvind Rajput <singhsubak9926@gmail.com>
2019-05-31 13:08:58 +0530

    voice abstract class

---
+++ mewadatoastsample/src/main/java/com/example/mewadatoastsample/VoiceClassIntegration.java
@@ -0,0 +1,16 @@
+package com.example.mewadatoastsample;
+
+import android.content.Context;
+
+abstract class VoiceClassIntegration extends MasterClass{
+
+    private static int noAccessUserThis(Context context,int calculation){
+        calculation  = 12 * 12;
+        return calculation;
+    }
+
+    public static  int accessUser(Context context, int cal){
+        cal =  12 * 10;
+        return cal;
+    }
+}
