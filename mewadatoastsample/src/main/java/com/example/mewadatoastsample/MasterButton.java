455238e45cb9f885f93ea26d0e2e547dd2ca01d6
Arvind Rajput <singhsubak9926@gmail.com>
2019-05-31 16:11:43 +0530

    otie animatioasdn asd  text view  ads

---
+++ mewadatoastsample/src/main/java/com/example/mewadatoastsample/MasterButton.java
@@ -0,0 +1,21 @@
+package com.example.mewadatoastsample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class MasterButton extends android.support.v7.widget.AppCompatButton {
+
+    public MasterButton(Context context) {
+        super(context);
+    }
+
+    public MasterButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MasterButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
